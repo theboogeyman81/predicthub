0c31b47f659ac71dd4c51d23588d52a4f97ed798
theboogeyman81 <theboogeyman8144@gmail.com>
2026-01-17 12:36:38 +0530

    code for login page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Register from './Register.jsx'
+
+function App() {
+
+  return (
+    <main className='App'>
+      <Register />
+    </main>
+  )
+
+}
+
+export default App
